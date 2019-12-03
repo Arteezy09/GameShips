2c6a3531d40724bc34aabbf19a126c701be86102
Arteezy09 <finalfantasy97@mail.ru>
2019-12-03 11:07:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,46 +12,36 @@ server.listen(port, () => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 var room = 0;
-
 var users = [];
 
 io.on('connection', (socket) => {
 
+    socket.on('join1', (data) => {
+      socket.join(++room);
+      users[room] = data.arr;
+      socket.emit('room', { room: room });
+    });
 
-  socket.on('join1', (data) => {
+    socket.on('join2', (data) => {
+      var room = io.nsps['/'].adapter.rooms[data.room];
+      if (room && room.length === 1) {
+        socket.join(data.room);
+        socket.broadcast.to(data.room).emit('newGame', { room: data.room, arr: data.arr, turn: 1 });
+        socket.emit('newGame', { room: data.room, arr: users[data.room], turn: 0 });
+      } 
+      else {
+        socket.emit('message', { message: 'Комнаты не существует или она заполнена!' });
+      }
+    });
 
-    socket.join(++room);
-    users[room] = data.arr;
-    socket.emit('room', { room: room });
-  });
+    socket.on('click', (data) => {
+      socket.broadcast.to(data.room).emit('click', { index: data.index, turn: data.turn });
+    });
 
-
-  socket.on('join2', (data) => {
-
-    var room = io.nsps['/'].adapter.rooms[data.room];
-    if (room && room.length === 1) {
-      socket.join(data.room);
-      socket.broadcast.to(data.room).emit('newGame', { room: data.room, arr: data.arr, turn: 1 });
-      socket.emit('newGame', { room: data.room, arr: users[data.room], turn: 0 });
-    } 
-    else {
-      socket.emit('message', { message: 'Комнаты не существует или она заполнена!' });
-    }
-  });
-
-
-  socket.on('click', (data) => {
-
-    socket.broadcast.to(data.room).emit('click', { index: data.index, turn: data.turn });
-  });
-
-  
-  socket.on('endGame', (data) => {
-
-    socket.broadcast.to(data.room).emit('endGame', { win: 0 });
-    socket.emit('endGame', { win: 1 });
-  });
-
+    socket.on('endGame', (data) => {
+      socket.broadcast.to(data.room).emit('endGame', { win: 0 });
+      socket.emit('endGame', { win: 1 });
+    });
 });
 
 
